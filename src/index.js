eecd51dd75789b1dd7b3c7c608064f49e9871b01
muhammadarslanapax <arslanapax@gmail.com>
2026-03-13 01:40:15 +0500

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ app.get("/start", async (req, res) => {
 });
 
 
+
+app.get("/test", async (req, res) => {
+
+    res.json({ "msg": "Welcome to testing" });
+});
+
+
+
 app.listen(port, () => {
     console.log("SERVER IS RUNNING AT " + port)
 })
